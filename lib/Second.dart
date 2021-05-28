1f8d8f482683f26590cca560bb56c7c65f7210dc
abs12seth <absseth1202@gmail.com>
2021-05-28 15:30:23 +0530

    Adding login UI

--- lib/Second.dart
+++ lib/Second.dart
@@ -14,11 +14,45 @@ class SecondState extends State<SecondScreen> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("CoWin"),
+        backgroundColor: Colors.teal,
+        title: Text("Sample"),
       ),
-      body: Container(
-        
-      ),
+      body:SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.teal,
+                        borderRadius: BorderRadius.circular(50.0)
+                    ),
+                  child: Center(
+                    child: Text("Sample",style: TextStyle(fontSize: 30.0,color: Colors.white),),
+                  )
+
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(left: 15,right: 15, top: 5,bottom: 0.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border:OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    labelText: 'Email',
+                    hintText: 'Enter a email ID',
+                  ),
+                ),
+            )
+            
+          ],
+        ),
+      )
     );
   }
 }
